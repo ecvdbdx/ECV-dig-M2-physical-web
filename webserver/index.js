a8ac0826eaf1d63c079aaf1715a20227eb4bbc3f
Clément Ollivier <clement.o2p@gmail.com>
2017-09-04 15:43:29 +0200

    :sparkles: Add socket.io client to webserver

--- webserver/index.js
+++ webserver/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+const settings = require('../settings.js');
 
 const app = express();
 
+// Creates Socket.io server to receive events from user's client
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+// Creates Socket.io client to emit events to piserver
+const clientio  = require('socket.io-client');
+const client    = clientio.connect(`${settings.PISERVER_ADDRESS}:${settings.PISERVER_PORT}`);
 
 express.static('static');
 
@@ -12,10 +18,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/static/index.html'));
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', (socket) => {
+  console.log('connected');
+  socket.on('message', (msg) => {
+    console.log('message: ' + msg);
+    // Route event to piserver
+    client.emit('piserver', msg);
+  });
 });
 
-http.listen('8080', () => {
-  console.log('Server listening on port 8080');
+http.listen(settings.WEBSERVER_PORT, () => {
+  console.log(`Server listening on port ${settings.WEBSERVER_PORT}`);
 });
