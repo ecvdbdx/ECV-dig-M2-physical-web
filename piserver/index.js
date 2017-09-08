351916c7fa8aee1f0450aeab4e01b28404dace4d
Simon <simon@MacBook-Pro.local>
2017-09-08 12:09:42 +0200

    Active pin for product

--- piserver/index.js
+++ piserver/index.js
@@ -1,19 +1,50 @@
 const express = require('express');
-const path    = require('path');
+const path = require('path');
 const settings = require('../settings.js');
+const rpio = require('rpio');
 
 const app = express();
 
+// Creates Socket.io server to receive events from user's client
 const http = require('http').Server(app);
-const io   = require('socket.io')(http);
+const io = require('socket.io')(http);
+
+// Creates Socket.io client to emit events to piserver
+const clientio = require('socket.io-client');
+const client = clientio.connect(`${settings.WEBSERVER_ADDRESS}:${settings.WEBSERVER_PORT}`);
+
+express.static('static');
 
 io.on('connection', (socket) => {
-  console.log('connected');
-  socket.on('piserver', (msg) => {
-    console.log('message: ' + msg);
-  });
-});
+  socket.on('products', (products) => {
+    switch (products) {
+      case 'A23':
+        rpio.open(7, rpio.OUTPUT, rpio.LOW);
+        rpio.write(7, rpio.HIGH);
+        rpio.sleep(5.2);
+        rpio.write(7, rpio.LOW);
+        break;
+      case 'A22':
+        rpio.open(11, rpio.OUTPUT, rpio.LOW);
+        rpio.write(11, rpio.HIGH);
+        rpio.sleep(5.2);
+        rpio.write(11, rpio.LOW);
+        break;
+      case 'A51':
+        rpio.open(13, rpio.OUTPUT, rpio.LOW);
+        rpio.write(13, rpio.HIGH);
+        rpio.sleep(5.2);
+        rpio.write(13, rpio.LOW);
+        break;
+      case 'A52':
+        rpio.open(15, rpio.OUTPUT, rpio.LOW);
+        rpio.write(15, rpio.HIGH);
+        rpio.sleep(5.2);
+        rpio.write(15, rpio.LOW);
+        break;
 
-http.listen(settings.PISERVER_PORT, '0.0.0.0', () => {
-  console.log(`Server listening on port ${settings.PISERVER_PORT}`);
+    }
+    const msg = 'Your product has been delivered';
+    client.emit('message', msg);
+  });
 });
