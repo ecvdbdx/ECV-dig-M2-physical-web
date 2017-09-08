8e6307c25c6d17b3e74dc955f72f7a19b9f0874f
Clément Ollivier <clement.o2p@gmail.com>
2017-09-08 10:57:14 +0200

    :sparkles: Move front to vue.js

---
+++ webserver/views/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <RCHeader></RCHeader>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import RCHeader from './components/RCHeader';
+
+export default {
+  name: 'app',
+  components: {
+    RCHeader,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
