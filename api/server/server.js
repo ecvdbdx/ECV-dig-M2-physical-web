558719de6900e3ca0e9b54b6c860edc63955e14c
Bram van Osta <bramvanosta@gmail.com>
2017-09-08 12:35:09 +0200

    Add Cors to API

--- api/server/server.js
+++ api/server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const api = require('./api/api');
 
+// Setup Cors
+app.use(cors());
 // Setup the api
 app.use('/api', api);
 // Setup the static files
