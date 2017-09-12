0ba77caf80933feb8aedcba789736958f69f1251
Bram van Osta <bramvanosta@gmail.com>
2017-09-12 12:30:31 +0200

    Update machine mapping with times

--- machine-mapping.js
+++ machine-mapping.js
@@ -3,96 +3,96 @@ module.exports = {
         name: 'A21',
         pin_line: 36,
         pin_row: 7,
-        duration: 3
+        duration: 5.28
     },
     A22: {
         name: 'A22',
         pin_line: 36,
         pin_row: 13,
-        duration: 3
+        duration: 5.45
     },
     A23: {
         name: 'A23',
         pin_line: 36,
         pin_row: 11,
-        duration: 3
+        duration: 5.45
     },
     A24: {
         name: 'A24',
         pin_line: 36,
         pin_row: 16,
-        duration: 3
+        duration: 5.28
     },
     A25: {
         name: 'A25',
         pin_line: 36,
         pin_row: 12,
-        duration: 3
+        duration: 5.4
     },
     A26: {
         name: 'A26',
         pin_line: 36,
         pin_row: 18,
-        duration: 3
+        duration: 5.35
     },
     A27: {
         name: 'A27',
         pin_line: 36,
         pin_row: 38,
-        duration: 3
+        duration: 5.35
     },
     A28: {
         name: 'A28',
         pin_line: 36,
         pin_row: 37,
-        duration: 3
+        duration: 5.38
     },
     A51: {
         name: 'A51',
         pin_line: 35,
         pin_row: 7,
-        duration: 3
+        duration: 5.21
     },
     A52: {
         name: 'A52',
         pin_line: 35,
         pin_row: 13,
-        duration: 3
+        duration: 5.22
     },
     A53: {
         name: 'A53',
         pin_line: 35,
         pin_row: 11,
-        duration: 3
+        duration: 5.45
     },
     A54: {
         name: 'A54',
         pin_line: 35,
         pin_row: 16,
-        duration: 3
+        duration: 5.37
     },
     A55: {
         name: 'A55',
         pin_line: 35,
         pin_row: 12,
-        duration: 3
+        duration: 5.4
     },
     A56: {
         name: 'A56',
         pin_line: 35,
         pin_row: 18,
-        duration: 3
+        duration: 5.4
     },
     A57: {
         name: 'A57',
         pin_line: 35,
         pin_row: 38,
-        duration: 3
+        duration: 5.4
     },
     A58: {
         name: 'A58',
         pin_line: 35,
         pin_row: 37,
-        duration: 3
+        duration: 5.4
     }
 };
